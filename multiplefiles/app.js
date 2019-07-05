2d475cab3fb30fd6c82f2e26b8820dc642ca78fa
Jeevan:M <csejeevan@gmail.com>
2019-07-05 22:45:28 +0530

    Worked on multiple file in file system

---
+++ multiplefiles/app.js
@@ -0,0 +1,5 @@
+const add=require('./util.js')
+
+const sum=add(5,6)
+
+console.log(sum)
---
+++ multiplefiles/util.js
@@ -0,0 +1,6 @@
+console.log('util.js')
+
+const add = function(a,b){
+    return a + b
+}
+module.exports = add
