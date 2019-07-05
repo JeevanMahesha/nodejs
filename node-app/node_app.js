837b48ce196d67ac77a998db6387dba97ef78f1d
Jeevan:M <csejeevan@gmail.com>
2019-07-05 22:07:28 +0530

    The day 2 worked filesystem using node

---
+++ node-app/node_app.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+
+// This is to create a new notepad file using node js and to write some text in that notepad 
+fs.writeFileSync('notes.txt','This File was created by Node.js!')
+
+// This is to append the text in existing notepad file  
+fs.appendFileSync('notes.txt','  The code writen by JEEVAN.M :)')
