9260d6df4e7afb428aacb2ec2d58cddabac9bdc9
unknown <csejeevanm@gmail.com>
2019-11-18 20:40:31 +0530

    Getting the input from the user using commend line

--- Getting_input_from_user/app.js
+++ Getting_input_from_user/app.js
@@ -1,3 +1,11 @@
+/* 
+process.argv is an array which gets the input from the user using commend line and printes it
+
+process.argv[2] in this the first two index will having the node path and file path so always start 
+with third index value in the array 
+
+*/
+
 const command = process.argv[2]
  if (command === 'add'){
     console.log('Adding Notes.......');
