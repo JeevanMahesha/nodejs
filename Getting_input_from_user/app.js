92d28e3798bb673c5381bc9a68dbc218c58f86e8
unknown <csejeevanm@gmail.com>
2019-11-19 14:10:23 +0530

    Worked on yargs npm package

--- Getting_input_from_user/app.js
+++ Getting_input_from_user/app.js
@@ -1,16 +1,50 @@
 /* 
-process.argv is an array which gets the input from the user using commend line and printes it
-
-process.argv[2] in this the first two index will having the node path and file path so always start 
-with third index value in the array 
+yargs package is used for give command in command line
 
 */
+const yargs = require('yargs')
 
-const command = process.argv[2]
- if (command === 'add'){
-    console.log('Adding Notes.......');
- } else if( command === 'remove'){
-     console.log('Removing Notes.......');
- }else{
-     console.log('Wrong Input');
- }
+// customize the yargs version
+yargs.version('9.0.0')
+
+//Create Command
+yargs.command({
+    command : 'add',
+    describe : 'Adding a note',
+    handler : function(){
+        console.log('Adding the new notes..........!');
+        
+    }
+})
+
+// Remove command
+yargs.command({
+    command : 'remove',
+    describe : 'Removing the notes',
+    handler : function(){
+        console.log('Removing the note...........!');
+        
+    }
+})
+
+// read command
+yargs.command({
+    command : 'read',
+    describe : 'Reading the notes',
+    handler : function(){
+        console.log('Reading the notes..........!');
+        
+    }
+})
+
+//list command
+yargs.command({
+    command : 'list',
+    describe :'List the notes',
+    handler : function(){
+        console.log('Listing the notes..........!');
+        
+    }
+})
+
+console.log(yargs.argv);
