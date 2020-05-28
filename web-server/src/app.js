f3824c119f895fd2e63d9c4bf621ae32486e9421
Jeevan-M <csejeevanm@gmail.com>
2020-05-28 20:25:00 +0530

    dynamic page render

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,8 +4,17 @@ const express = require('express')
 const app = express()
 const pathForHTMLPage = path.join(__dirname, '../public')
 
-
 app.use(express.static(pathForHTMLPage))
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'hbs');
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Jeevan'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
@@ -13,7 +22,6 @@ app.get('/weather', (req, res) => {
         location: 'Mysore'
     })
 })
-
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
