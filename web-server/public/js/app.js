c3427bf8492ed133cd98c116a08f716c13fa28e0
Jeevan-M <csejeevanm@gmail.com>
2020-05-29 21:38:54 +0530

    working with fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,11 @@
 console.log('this is javascript');
+
+fetch('http://localhost:3000/weather?address=!')
+    .then((res) => res.json())
+    .then((data) => {
+        if (data.Error) {
+            console.log(data);
+        } else {
+            console.log(data);
+        }
+    })
