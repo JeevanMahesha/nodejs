53dad513216ed8dd9bc62d4e1957fcd6e9bd2b19
Jeevan-M <csejeevanm@gmail.com>
2020-05-30 00:12:34 +0530

    changed to bootstrap

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,15 @@
-console.log('this is javascript');
+const weatherform = document.querySelector('form')
+const searchvalue = document.querySelector('input')
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
+    fetch('http://localhost:3000/weather?address=' + searchvalue.value)
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.Error) {
+                console.log(data);
+            } else {
+                console.log(data);
+            }
+        })
 
-fetch('http://localhost:3000/weather?address=!')
-    .then((res) => res.json())
-    .then((data) => {
-        if (data.Error) {
-            console.log(data);
-        } else {
-            console.log(data);
-        }
-    })
+})
