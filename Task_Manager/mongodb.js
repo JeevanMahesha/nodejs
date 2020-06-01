49c8246ac32a0def873a86c7b2b8c62b8f17c7b3
Jeevan-M <csejeevanm@gmail.com>
2020-06-01 20:35:19 +0530

    get the data from the db

--- Task_Manager/mongodb.js
+++ Task_Manager/mongodb.js
@@ -1,10 +1,10 @@
-const mongodb = require('mongodb')
-const mongoclient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
+
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager-db'
 
-mongoclient.connect(connectionURL, { 'useNewUrlParser': true }, (error, client) => {
+MongoClient.connect(connectionURL, { 'useNewUrlParser': true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect the Database');
     }
@@ -22,19 +22,51 @@ mongoclient.connect(connectionURL, { 'useNewUrlParser': true }, (error, client) 
     // })
 
     //=======    INSERT MANY RECORD TO DB  ==========
-    db.collection('task').insertMany([{
-        description: "this is first record",
-        completed: true
-    }, {
-        description: "this is secound record",
-        completed: false
-    }, {
-        description: "this is third record",
-        completed: true
-    }], (error, result) => {
+    // db.collection('task').insertMany([{
+    //     description: "this is first record",
+    //     completed: true
+    // }, {
+    //     description: "this is secound record",
+    //     completed: false
+    // }, {
+    //     description: "this is third record",
+    //     completed: true
+    // }], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert record');
+    //     }
+    //     console.log(result.ops);
+    // })
+
+
+    //====== find the one record from the database
+    // db.collection('user').findOne({ _id: new ObjectID("5ed4b2f4682e59220c5081dd") }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch data');
+    //     }
+    //     console.log(result);
+    // })
+
+    //====== find the all record which match 
+    // db.collection('user').find({ age: 23 }).toArray((error, user) => {
+    //     console.log(user)
+    // })
+
+    //==== find the count of record which match
+    // db.collection('user').find({ age: 23 }).count((error, userCount) => {
+    //     console.log(userCount)
+    // })
+
+
+    db.collection('task').findOne({ _id: new ObjectID("5ed4b7ba8ce52a209c1d340d") }, (error, result) => {
         if (error) {
-            return console.log('Unable to insert record');
+            console.log('unable to find');
         }
-        console.log(result.ops);
+        console.log(result);
     })
+
+    db.collection('task').find({ completed: false }).toArray((error, result) => {
+        console.log(result);
+    })
+
 })
