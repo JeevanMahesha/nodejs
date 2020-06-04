aeee35431a0c2123600c98166d47ef75409dda90
Jeevan-M <csejeevanm@gmail.com>
2020-06-04 11:51:14 +0530

    working with http status code and spliting the working modules

--- Task-Manager-App/src/index.js
+++ Task-Manager-App/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
-require("../src/db/mangoose")
+require("./db/db_connect_mangoose")
 const User = require("../src/models/user")
+const Task = require("../src/models/task")
 
 
 const app = express()
@@ -11,12 +12,21 @@ app.use(express.json())
 app.post("/user", (req, res) => {
     const user = new User(req.body)
     user.save().then(() => {
-        res.send(user)
-    }).catch(() => {
+        res.status(201).send(user)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
 
+app.post("/task", (req, res) => {
+    const task = new Task(req.body)
+    task.status(201).save().then(() => {
+        res.send(task)
+    }).catch((error) => {
+        res.status(400).send(error)
     })
 })
 
 app.listen(port, () => {
-    console.log('Server is up on port' + port);
+    console.log('Server is up on port ' + port);
 })
--- Task-Manager-App/src/models/task.js
+++ Task-Manager-App/src/models/task.js
@@ -1,14 +1,5 @@
 const mongoose = require("mongoose")
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-db', {
-    useNewUrlParser: true,
-    useCreateIndex: true
-})
-
-
-
-
 const Task = mongoose.model('Task', {
     description: {
         type: String,
@@ -20,3 +11,5 @@ const Task = mongoose.model('Task', {
         default: false
     }
 })
+
+module.exports = Task
