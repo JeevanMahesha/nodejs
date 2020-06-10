c3e1828e52635b89c20036b5f6abcf849be228ad
Jeevan-M <csejeevanm@gmail.com>
2020-06-10 22:25:54 +0530

    hiding the data or passing the require data for the user

--- Task-Manager-App/src/models/userModel.js
+++ Task-Manager-App/src/models/userModel.js
@@ -49,6 +49,13 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
+userSchema.methods.toJSON = function() {
+    const userObject = this.toObject()
+    delete userObject.password
+    delete userObject.tokens
+    return userObject
+}
+
 userSchema.methods.generateAuthToken = async function() {
     const token = jwt.sign({ _id: this._id.toString() }, 'TaskManagerApp')
     this.tokens = this.tokens.concat({ token })
