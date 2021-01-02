f6628e3fa43e8465752b71755a466823847992b6
Jeevan-M <csejeevanm@gmail.com>
2021-01-02 19:17:43 +0530

    all fixed

--- hello.js
+++ hello.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+// console.log("Hello World")
+// run node filename.js to see the output
+
+const fs = require('fs');
+const path = require('path');
+fileNameArray = fs.readdirSync(__dirname);
+fileNameToDelete = 'node_modules'
+fileNameArray.forEach(element => {
+    let data = path.join(element, fileNameToDelete)
+    if (fs.existsSync(data)) {
+        fs.rmdirSync(data, { recursive: true })
+        console.log(data);
+    }
+});
