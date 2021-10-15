8cb26524bfeb9beb00d4e4184ebf317e2c54fef7
Jeevan M <csejeevanm@gmail.com>
2021-10-15 19:39:07 +0530

    final etcd

--- etcd/etcd.js
+++ etcd/etcd.js
@@ -3,15 +3,14 @@ const { Etcd3 } = require("etcd3");
 const client = new Etcd3();
 
 const watchFooValue = async (key) => {
-    return await
-        client
-            .watch()
-            .key(key)
-            .create();
+    return client
+        .watch()
+        .key(key)
+        .create();
 }
 
 const getFooValue = async (key) => {
-    return await client.get(key).string()
+    return client.get(key).string()
 }
 
 module.exports = {
--- etcd/index.js
+++ etcd/index.js
@@ -5,23 +5,19 @@ const app = express();
 
 app.use(express.json())
 
-let fooValue
-key = "foo"
+let fooValue = "No Foo Value is updated"
+let key = "foo"
 
 app.get('/', async (req, res) => {
-    fooValue = await etcdGetAndWatch.getFooValue(key)
-    await etcdGetAndWatch.watchFooValue(key).then(
-        (watcher) => {
-            watcher.on("put", res => {
-                if (res.value.toString()) {
-                    fooValue = res.value.toString()
-                    console.log(res.value.toString());
-                }
-            })
-        }
-    )
+    fooValue = await etcdGetAndWatch.getFooValue(key);
+    const watcher = await etcdGetAndWatch.watchFooValue(key);
+    watcher.on("put", watcherPut)
     res.send('This is Foo Value : ' + fooValue)
 });
+
+function watcherPut(watchValue) {
+    fooValue = watchValue.value.toString();
+}
 
 setInterval(() => {
     console.log(fooValue);
