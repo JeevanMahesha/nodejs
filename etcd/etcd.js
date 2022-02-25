45d849e12aeda615ae8a8fffd53c423635c92c98
Jeevan M <csejeevanm@gmail.com>
2022-02-25 11:00:30 +0530

    connecting to external ETCD cluster

--- etcd/etcd.js
+++ etcd/etcd.js
@@ -1,19 +1,28 @@
 const { Etcd3 } = require("etcd3");
 
-const etcdClient = new Etcd3();
+let options = {
+	auth: {
+		username: "USERNAME",
+		password: "PASSWORD",
+	},
+	hosts: ["IP:PORT"],
+};
+const etcdClient = new Etcd3(options);
+const pocName = etcdClient.namespace("PATH_IF_NEEDED");
 
 const watchTheKeyValueChange = async (key) => {
-    return etcdClient
-        .watch()
-        .key(key)
-        .create();
-}
+	return pocName.watch().key(key).create();
+};
 
 const getValueFromETCD = async (key) => {
-    return etcdClient.get(key).string()
-}
+	return pocName.get(key).string();
+};
+
+const putValuetoETCD = async (key, value) => {
+	await pocName.put(key).value(value);
+};
 
 module.exports = {
-    watchTheKeyValueChange,
-    getValueFromETCD
-}
+	watchTheKeyValueChange,
+	getValueFromETCD,
+};
