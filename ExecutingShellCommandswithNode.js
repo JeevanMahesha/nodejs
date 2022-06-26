c4b986e1ad28f692cd136704c9d63531c0938692
Jeevan M <csejeevanm@gmail.com>
2022-06-26 18:41:06 +0530

    script are updated

--- ExecutingShellCommandswithNode.js
+++ ExecutingShellCommandswithNode.js
@@ -1,8 +1,9 @@
 const { exec } = require("child_process");
 const fs = require("fs");
 const path = require("path");
+const PACKAGE_JSON = require("./remove_Node_Modules");
 fs.readdirSync(__dirname).forEach((folderName) => {
-	if (!folderName.includes(".")) {
+	if (!folderName.includes(".") && path.join(folderName, PACKAGE_JSON)) {
 		console.log(folderName);
 		exec("npm i --prefix ./" + folderName);
 	}
--- remove_Node_Modules.js
+++ remove_Node_Modules.js
@@ -1,10 +1,24 @@
-
-const fs = require('fs');
-const path = require('path');
-fs.readdirSync(__dirname).forEach(element => {
-    let data = path.join(element, 'node_modules')
-    if (fs.existsSync(data)) {
-        fs.rmdirSync(data, { recursive: true })
-        console.log(data);
-    }
+const fs = require("fs");
+const path = require("path");
+const PACKAGE_LOCK_JSON = "package-lock.json";
+const PACKAGE_JSON = "package.json";
+const isPackageLockJson = true;
+fs.readdirSync(__dirname).forEach((folderName) => {
+	let data = path.join(folderName, "node_modules");
+	if (!folderName.includes(".") && path.join(folderName, PACKAGE_JSON)) {
+		if (isPackageLockJson) {
+			let package_lock_json = path.join(folderName, PACKAGE_LOCK_JSON);
+			if (fs.existsSync(package_lock_json)) {
+				// console.log(package_lock_json);
+				fs.unlinkSync(package_lock_json, { recursive: true });
+			}
+		}
+		if (fs.existsSync(data)) {
+			fs.rmSync(data, { recursive: true });
+			// console.log(data);
+		}
+	}
 });
+console.log("DONE");
+
+module.exports = PACKAGE_JSON;
