50dd25330c21be13e01a3ff04abc9e318dc4ba53
Jeevan M <csejeevanm@gmail.com>
2022-07-02 19:47:52 +0530

    callback in TS

--- TypeScript/TS-initial-Project/src/Functions/index.ts
+++ TypeScript/TS-initial-Project/src/Functions/index.ts
@@ -8,3 +8,16 @@ function calculateTax(income: number, taxYear = 2022): number {
 }
 
 calculateTax(10000, 2000);
+
+const printResult = (result: number): void => console.log(result);
+
+function add(
+	number1: number,
+	number2: number,
+	callBackFun: (result: number) => void
+) {
+	let finalValue = number1 + number2;
+	callBackFun(finalValue);
+}
+
+add(5, 10, printResult);
