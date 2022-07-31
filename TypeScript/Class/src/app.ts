b8ea2c7b77f3b05f4413840caaaf135bbac12605
Jeevan <csejeevanm@gmail.com>
2022-07-31 11:49:01 +0530

    access modifier

--- TypeScript/Class/src/app.ts
+++ TypeScript/Class/src/app.ts
@@ -1,10 +1,23 @@
 class Department {
 	name: string;
+	private employees: string[] = [];
 
 	constructor(userName: string) {
 		this.name = userName;
 	}
+
+	addEmployee(employee: string): void {
+		this.employees.push(employee);
+	}
+
+	printEmployeeInformation() {
+		console.log(this.employees.length);
+		console.log(this.employees);
+	}
 }
 
 const userOne = new Department("Jeevan");
-console.log(userOne);
+
+userOne.addEmployee("EMP1");
+userOne.addEmployee("EMP2");
+userOne.printEmployeeInformation();
