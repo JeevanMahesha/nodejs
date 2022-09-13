ce558f9e9a1dcc3eb1dab82644b793e55a1fe673
Jeevan <csejeevanm@gmail.com>
2022-09-13 21:03:56 +0530

    e 04

--- TypeScript/TS-challenges/src/04-optional-params.problem.ts
+++ TypeScript/TS-challenges/src/04-optional-params.problem.ts
@@ -1,20 +1,20 @@
 import { expect, it } from "vitest";
 
-export const getName = (first: string, last: string) => {
-  if (last) {
-    return `${first} ${last}`;
-  }
-  return first;
+export const getName = (first: string, last?: string) => {
+	if (last) {
+		return `${first} ${last}`;
+	}
+	return first;
 };
 
 it("Should work with just the first name", () => {
-  const name = getName("Matt");
+	const name = getName("Matt");
 
-  expect(name).toEqual("Matt");
+	expect(name).toEqual("Matt");
 });
 
 it("Should work with the first and last name", () => {
-  const name = getName("Matt", "Pocock");
+	const name = getName("Matt", "Pocock");
 
-  expect(name).toEqual("Matt Pocock");
+	expect(name).toEqual("Matt Pocock");
 });
