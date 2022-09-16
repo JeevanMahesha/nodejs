d551a12820f6011c76842bd4ddd66b399f9e9add
Jeevan <csejeevanm@gmail.com>
2022-09-16 15:41:04 +0530

    Interfaces as Function Types

--- TypeScript/Interface/src/app.ts
+++ TypeScript/Interface/src/app.ts
@@ -20,3 +20,15 @@ class Person implements IPerson {
 		console.info(this.name + " Welcome");
 	}
 }
+
+type addFun = (a: number, b: number) => number;
+
+interface IAddFun {
+	(a: number, b: number): number;
+}
+
+let addFunction: addFun;
+let IAddFunction: IAddFun;
+
+addFunction = (n1: number, n2: number) => n1 + n2;
+IAddFunction = (n1: number, n2: number) => n1 + n2;
