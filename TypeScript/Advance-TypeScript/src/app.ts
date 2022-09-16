59ceb6b7da4049f3d8d20508cdf65a30342598c2
Jeevan <csejeevanm@gmail.com>
2022-09-16 16:23:12 +0530

    Intersection

--- TypeScript/Advance-TypeScript/src/app.ts
+++ TypeScript/Advance-TypeScript/src/app.ts
@@ -1,1 +1,38 @@
 // Code goes here!
+
+type Admin = {
+	name: string;
+	privileges: string[];
+};
+
+type Employee = {
+	name: string;
+	startDate: Date;
+};
+
+// ? Intersection type
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+	name: "Max",
+	privileges: ["create-server"],
+	startDate: new Date(),
+};
+
+interface IAdmin {
+	name: string;
+	privileges: string[];
+}
+
+interface IEmployee {
+	name: string;
+	startDate: Date;
+}
+
+interface IElevatedEmployee extends IAdmin, IEmployee {}
+
+const e2: ElevatedEmployee = {
+	name: "jeevan",
+	privileges: ["create-server"],
+	startDate: new Date(),
+};
