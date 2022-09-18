94c9cdd40dad390e66076856383c8a677d4dc6a8
Jeevan <csejeevanm@gmail.com>
2022-09-18 13:27:04 +0530

    Creating a Generic Function

--- TypeScript/Advance-TypeScript/src/app.ts
+++ TypeScript/Advance-TypeScript/src/app.ts
@@ -1,7 +1,25 @@
 const names: Array<string> = []; // string[]
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
+const promise: Promise<string> = new Promise((resolve, _reject) => {
 	setTimeout(() => {
 		resolve("This is Finished");
 	}, 2000);
 });
+
+// =============================================================================================
+
+function merge<T extends object, U>(objectA: T, objectB: U): T & U {
+	return Object.assign(objectA, objectB);
+}
+
+/* 
+
+? This is once way 
+
+function merge<T, U>(objectA: T, objectB: U): T & U {
+	return Object.assign({}, objectA, objectB);
+}
+ */
+const mergedObj = merge({ name: "NAME" }, { age: 20 });
+
+console.log(mergedObj);
