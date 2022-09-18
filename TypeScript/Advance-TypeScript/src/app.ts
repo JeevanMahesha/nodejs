fbc5a11d4e4fd2221569bc2bdb16570b3c208649
Jeevan <csejeevanm@gmail.com>
2022-09-18 12:17:33 +0530

    Function Overload

--- TypeScript/Advance-TypeScript/src/app.ts
+++ TypeScript/Advance-TypeScript/src/app.ts
@@ -137,3 +137,13 @@ userInputMessage.value = "Hi everyone";
 interface ErrorContainer {
 	[prop: string]: string;
 }
+
+// Function Overload
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: any, b: any) {
+	if (typeof a === "string" && typeof b === "string") {
+		return [a, b].join();
+	}
+	return a + b;
+}
