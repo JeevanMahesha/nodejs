9fb84e41ca88ddd3f77958ba2f98a0f73b94aa1b
Jeevan <csejeevanm@gmail.com>
2022-09-19 10:34:43 +0530

    Doc added

--- TypeScript/Advance-TypeScript/src/app.ts
+++ TypeScript/Advance-TypeScript/src/app.ts
@@ -1,3 +1,15 @@
+/**
+ * "The Logger function takes a string and returns a function that takes a constructor and logs the
+ * string."
+ *
+ * The Logger function is a factory function that returns a function.
+ *
+ * The returned function is a decorator.
+ *
+ * The decorator takes a constructor and logs the string
+ * @param {string} logString - string - This is the string that we want to log out.
+ * @returns A function that takes a constructor function as an argument.
+ */
 function Logger(logString: string) {
 	return (constructor: Function) => {
 		console.log(logString);
@@ -5,7 +17,16 @@ function Logger(logString: string) {
 	};
 }
 
-function WithTemplet(templet: string, hookId: string) {
+/**
+ * It takes a template string and a hookId string as arguments, and returns a function that takes a
+ * constructor as an argument
+ * @param {string} templet - string - The HTML template to use.
+ * @param {string} hookId - The id of the element in the DOM that we want to render the component to.
+ * @returns A function that takes a constructor and returns a function that takes a constructor.
+ */
+function WithTemplate(templet: string, hookId: string) {
+	console.log("withTemplet");
+
 	return (constructor: any) => {
 		const hookEle = document.getElementById(hookId);
 		if (hookEle) {
@@ -17,7 +38,7 @@ function WithTemplet(templet: string, hookId: string) {
 }
 
 @Logger("Person")
-@WithTemplet("<h1>My Person Object</h1>", "app")
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
 	name = "Jeevan";
 	constructor() {
