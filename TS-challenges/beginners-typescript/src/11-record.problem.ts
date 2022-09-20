563b272e74f13fb8eb1a39f5a8f82654931b3cfa
Jeevan <csejeevanm@gmail.com>
2022-09-20 12:05:00 +0530

    e 11

--- TS-challenges/beginners-typescript/src/11-record.problem.ts
+++ TS-challenges/beginners-typescript/src/11-record.problem.ts
@@ -1,36 +1,37 @@
 import { expect, it } from "vitest";
+import { string } from "zod";
 
 const createCache = () => {
-  const cache = {};
+	const cache: { [key: string]: string } = {};
 
-  const add = (id: string, value: string) => {
-    cache[id] = value;
-  };
+	const add = (id: string, value: string) => {
+		cache[id] = value;
+	};
 
-  const remove = (id: string) => {
-    delete cache[id];
-  };
+	const remove = (id: string) => {
+		delete cache[id];
+	};
 
-  return {
-    cache,
-    add,
-    remove,
-  };
+	return {
+		cache,
+		add,
+		remove,
+	};
 };
 
 it("Should add values to the cache", () => {
-  const cache = createCache();
+	const cache = createCache();
 
-  cache.add("123", "Matt");
+	cache.add("123", "Matt");
 
-  expect(cache.cache["123"]).toEqual("Matt");
+	expect(cache.cache["123"]).toEqual("Matt");
 });
 
 it("Should remove values to the cache", () => {
-  const cache = createCache();
+	const cache = createCache();
 
-  cache.add("123", "Matt");
-  cache.remove("123");
+	cache.add("123", "Matt");
+	cache.remove("123");
 
-  expect(cache.cache["123"]).toEqual(undefined);
+	expect(cache.cache["123"]).toEqual(undefined);
 });
