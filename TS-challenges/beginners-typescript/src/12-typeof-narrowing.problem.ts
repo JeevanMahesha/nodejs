106d4a48954c22e914aea7583822d4064306b312
Jeevan <csejeevanm@gmail.com>
2022-09-20 12:14:13 +0530

    e 12

--- TS-challenges/beginners-typescript/src/12-typeof-narrowing.problem.ts
+++ TS-challenges/beginners-typescript/src/12-typeof-narrowing.problem.ts
@@ -1,11 +1,30 @@
 import { expect, it } from "vitest";
 
-const coerceAmount = (amount: number | { amount: number }) => {};
+interface IAmount {
+	amount: number;
+}
+
+const coerceAmount = (amount: number | IAmount): number | IAmount => {
+	if (typeof amount === "number") {
+		return amount;
+	} else {
+		return amount.amount;
+	}
+
+	// console.log(amount);
+
+	// if ("amount" in (amount as IAmount)) {
+	// 	let { amount: value } = <IAmount>(<unknown>amount);
+
+	// 	return value;
+	// }
+	// return amount;
+};
 
 it("Should return the amount when passed an object", () => {
-  expect(coerceAmount({ amount: 20 })).toEqual(20);
+	expect(coerceAmount({ amount: 20 })).toEqual(20);
 });
 
 it("Should return the amount when passed a number", () => {
-  expect(coerceAmount(20)).toEqual(20);
+	expect(coerceAmount(20)).toEqual(20);
 });
