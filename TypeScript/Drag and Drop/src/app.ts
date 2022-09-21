d2844fc026092c7b77fa0fd48592adb4893ec44b
Jeevan <csejeevanm@gmail.com>
2022-09-21 15:30:44 +0530

    DOM Element Selection & OOP Rendering

--- TypeScript/Drag and Drop/src/app.ts
+++ TypeScript/Drag and Drop/src/app.ts
@@ -1,1 +1,26 @@
 // Code goes here!
+
+class ProjectInput {
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element!: HTMLFormElement;
+
+	constructor() {
+		this.templateElement = <HTMLTemplateElement>(
+			document.getElementById("project-input")!
+		);
+		this.hostElement = <HTMLDivElement>document.getElementById("app")!;
+		const importedNode = document.importNode(
+			this.templateElement.content,
+			true
+		);
+		this.element = importedNode.firstElementChild as HTMLFormElement;
+		this.attach();
+	}
+
+	private attach() {
+		this.hostElement.insertAdjacentElement("afterbegin", this.element);
+	}
+}
+
+const prjInput = new ProjectInput();
